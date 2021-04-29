61fc5eb5c7381d771b6f9de0d3a37b009cb6140b
JadeXTGitHub <78270947+JadeXTGitHub@users.noreply.github.com>
2021-04-29 12:37:42 -0500

    Add files via upload

--- default.js
+++ default.js
@@ -10,8 +10,8 @@ var sticky = topnav.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    topnav.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    topnav.classList.remove("sticky");
   }
 }
